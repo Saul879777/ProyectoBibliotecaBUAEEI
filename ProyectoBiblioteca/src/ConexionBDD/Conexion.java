05814a90b10fc180f8f1c60e3ff2554e17d29c04
Saul879777 <saul879777@hotmail.com>
2016-04-26 19:32:08 -0500

    Archivos del proyecto original

---
+++ ProyectoBiblioteca/src/ConexionBDD/Conexion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConexionBDD;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author x360
+ */
+public class Conexion {
+  Connection con = null;
+  
+  public Connection conectar() {
+    try{
+      Class.forName("com.mysql.jdbc.Driver");
+      con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/biblioteca", "ricardo", "ricardo");
+      JOptionPane.showMessageDialog(null, "Conexión está establecida");
+    }
+    catch (ClassNotFoundException | SQLException e) {
+      JOptionPane.showMessageDialog(null, "Error de conexión \n"+e);
+    }
+    return con;
+  }
+}
